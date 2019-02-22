e1720e4b9b4343892147c5626250fe4a9ab23c33
BL-Madhuri-Patil <46067185+BL-Madhuri-Patil@users.noreply.github.com>
2019-02-22 18:33:30 +0530

    Update user-list.component.ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
+  //userList component constructor
   constructor() { }
 
   ngOnInit() {
